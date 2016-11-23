42da6678cf8b5192108cfaaa23c4c0ac99d44470
pieniacy <karol.pieniacy@gmail.com>
2016-11-23 23:00:40 +0100

    it doesnt work

--- citizen_test.cc
+++ citizen_test.cc
@@ -1,3 +1,8 @@
+#include <cstdio>
 #include "citizen.h"
 int main()
-{}
+{
+	Teenager<int> teen1(123, 13);
+	Sheriff<int> sheriff1(123, 42);
+	printf("%d %d %d\n", teen1.getAge(), sheriff1.getAge(), sheriff1.getAttackPower());
+}
