fb2b97a3bef78db647ccb08ed48225af6672706e
Marcin Chrzanowski <marcin.j.chrzanowski@gmail.com>
2016-11-24 20:01:34 +0100

    Test monster constructor

--- monsterTest.cc
+++ monsterTest.cc
@@ -1,12 +1,26 @@
 #include "citizen.h"
 #include "monster.h"
+#include "testing.h"
+
+void testMonsterConstructor() {
+    beginTest();
+
+    Zombie<int> z(100, 2);
+    checkEqual(z.getHealth(), 100, "Int health set correctly.");
+    checkEqual(z.getAttackPower(), 2, "Int power set correctly.");
+
+    Mummy<double> m(50.42, 12.0);
+    checkEqual(m.getHealth(), 50.42, "Double health set correctly.");
+    checkEqual(m.getAttackPower(), 12.0, "Double power set correctly.");
+
+    Vampire<char> v(13, 12);
+    checkEqual<char>(v.getHealth(), 13, "Char health set correctly.");
+    checkEqual<char>(v.getAttackPower(), 12, "Char power set correctly.");
+
+    finishTest();
+}
 
 int main()
 {
-	Zombie<int> z1(100, 2);
-	Mummy<double> m1(100., 1.);
-	Vampire<unsigned char> v1(100, 5);
-
-	Teenager<int> teen1(123, 13);
-	attack(z1, teen1);
+    testMonsterConstructor();
 }
