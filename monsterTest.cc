afc36848565f7b99fda6b62e00d3d12cb8e499bb
pieniacy <karol.pieniacy@gmail.com>
2016-11-24 13:26:23 +0100

    Simple monsterTest

--- monsterTest.cc
+++ monsterTest.cc
@@ -1,6 +1,12 @@
+#include "citizen.h"
 #include "monster.h"
 
 int main()
 {
+	Zombie<int> z1(100, 2);
+	Mummy<double> m1(100., 1.);
+	Vampire<unsigned char> v1(100, 5);
 
+	Teenager<int> teen1(123, 13);
+	attack(z1, teen1);
 }
