5c8a177e955c489ce5b3bf2bc54fb88b25ea0d3e
pieniacy <karol.pieniacy@gmail.com>
2016-11-27 22:36:05 +0100

    Working tick(), genFibs is still in runtime

--- smalltown.h
+++ smalltown.h
@@ -1,7 +1,9 @@
 #ifndef SMALLTOWN_H_
 #define SMALLTOWN_H_
 
-#include <list>
+#include <algorithm>
+#include <array>
+#include <iostream>
 #include <string>
 #include <tuple>
 #include <type_traits>
@@ -18,43 +20,39 @@ class SmallTown {
 
 public:
     SmallTown(M monster, C... citizens) : monster_(monster),
-                                          citizens_(citizens...) {}
+                                          citizens_(citizens...) {} //ASK sprawdzenie czy są Citizen
+
     std::tuple<std::string, typename M::valueType, size_t> getStatus() {
         return std::make_tuple(getMonsterName(monster_), monster_.getHealth(),
-                               number_living_citizens);
+                               number_living_citizens_);
     }
+
+    void tick(U timestep)
+	{
+		if (monster_.getHealth() == 0) {
+			if (number_living_citizens_ == 0)
+				std::cout << "DRAW\n";
+			else
+				std::cout << "CITIZENS WON\n";
+			return;
+		} else if (number_living_citizens_ == 0) {
+			std::cout << "MONSTER WON\n";
+			return;
+		}
+
+		if (std::binary_search(fibs().begin(), fibs().end(), current_time_))
+			attackAll();
+
+		current_time_ += timestep;
+		current_time_ %= end_of_day + 1;
+	}
 private:
-	constexpr static std::list<U> genFibs()
-	{
-		U first = 0, second = 1;
-		while (first < start_time) {
-			U tmp = first + second;
-			first = second;
-			second = tmp;
-		}
-		if (first > end_of_day)
-			return std::list<U>();
-
-		std::list<U> res({first});
-		while (second <= end_of_day) {
-			res.push_back(second);
-			U tmp = first + second;
-			first = second;
-			second = tmp;
-		}
-		return res;
-	}
-	const std::list<U>& fibs() const
-	{
-		static std::list<U> generatedOnce(genFibs());
-		return generatedOnce;
-	}
-
-    U current_time = start_time;
+    U current_time_ = start_time;
     M monster_;
     std::tuple<C...> citizens_;
-    size_t number_citizens = std::tuple_size<std::tuple<C...>>::value;
-    size_t number_living_citizens = number_citizens;
+    size_t number_citizens_ = std::tuple_size<std::tuple<C...>>::value;
+    size_t number_living_citizens_ = number_citizens_;
+
     template<typename T, int ID>
     static std::string getMonsterName(Monster<T, ID> const &m) {
         return "Monster";
@@ -71,6 +69,45 @@ private:
     static std::string getMonsterName(Monster<T, 3> const &m) {
         return "Mummy";
     }
+
+    template<size_t I = 0>
+    inline typename std::enable_if_t<I == sizeof...(C)> attackAll()
+	{}
+	template<size_t I = 0>
+	inline typename std::enable_if_t<I < sizeof...(C)> attackAll()
+	{
+		bool wasAlive = std::get<I>(citizens_).getHealth() != 0;
+		attack(monster_, std::get<I>(citizens_));
+		if (wasAlive && std::get<I>(citizens_).getHealth() == 0)
+			--number_living_citizens_;
+		attackAll<I + 1>();
+	}
+
+	constexpr static size_t fibsNumber(size_t f, size_t s, size_t r)
+	{
+		return ((s > end_of_day) || (s < f) ? r : fibsNumber(s, f + s, r + 1));
+	}
+	constexpr static size_t fibsNumber() {return fibsNumber(0, 1, 1);}
+
+    constexpr static std::array<U, fibsNumber()> genFibs()
+	{
+		U first = 0, second = 1;
+		std::array<U, fibsNumber()> res;
+		size_t idx = 0;
+		res[idx++] = first;
+		while (second <= end_of_day && second >= first) {
+			res[idx++] = second;
+			U tmp = first + second;
+			first = second;
+			second = tmp;
+		}
+		return res;
+	}
+	static const std::array<U, fibsNumber()>& fibs()
+	{
+		static std::array<U, fibsNumber()> generatedOnce(genFibs()); //ASK w czasie kompilacji?
+		return generatedOnce;
+	}
 };
 
 #endif
--- smalltown_test.cc
+++ smalltown_test.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <tuple>
 
 #include "smalltown.h"
@@ -16,7 +17,7 @@ void testConstructor() {
         town2(Vampire<double>(3.14, 4.13), Teenager<float>(0.03, 12.32),
               Sheriff<unsigned>(12, 23, 3), Adult<long>(41, 21));
 
-    
+
     finishTest();
 }
 
@@ -37,13 +38,56 @@ void testGetStatus() {
             "Initial town1 status correct.");
 
     checkEqual(town2.getStatus(), std::make_tuple<std::string, double, size_t>("Zombie", 3.14, 3),
-            "Initial town1 status correct.");
+            "Initial town2 status correct.");
 
     finishTest();
 }
 
+void testTick() {
+    beginTest();
+
+    SmallTown<Vampire<int>, unsigned, 0, 4, Teenager<int>, Adult<int>,
+    Sheriff<int>>
+    town1(Vampire<int>(3, 4), Teenager<int>(13, 12), Adult<int>(41, 21),
+          Sheriff<int>(12, 23, 3));
+
+    checkEqual(town1.getStatus(), std::make_tuple<std::string, int, size_t>("Vampire", 3, 3),
+               "Initial town1 status correct.");
+
+    town1.tick(1);
+
+    checkEqual(town1.getStatus(), std::make_tuple<std::string, int, size_t>("Vampire", 0, 3),
+               "town1 after tick(1) status correct.");
+
+    std::cout << "Citizens should win:\n";
+    town1.tick(1);
+
+    SmallTown<Zombie<double>, unsigned char, 42, 255, Teenager<float>,
+    Sheriff<unsigned>, Adult<long>>
+    town2(Zombie<double>(3.14, 4.13), Teenager<float>(0.03, 12.32),
+          Sheriff<unsigned>(12, 23, 3), Adult<long>(41, 21));
+
+    checkEqual(town2.getStatus(), std::make_tuple<std::string, double, size_t>("Zombie", 3.14, 3),
+               "Initial town2 status correct.");
+
+    town2.tick(1);
+    checkEqual(town2.getStatus(), std::make_tuple<std::string, double, size_t>("Zombie", 3.14, 3),
+               "town2 after tick(1) status correct.");
+
+    town2.tick(12);
+    checkEqual(town2.getStatus(), std::make_tuple<std::string, double, size_t>("Zombie", 3.14, 3),
+               "town2 after tick(12) status correct.");
+
+    town2.tick(1);
+    //0.14 is probably not represantable by double, need checkEqual with tolerance
+    checkEqual(town2.getStatus(), std::make_tuple<std::string, double, size_t>("Zombie", 0.14000000000000012, 2),
+               "town2 after tick(1) status correct.");
+
+    finishTest();
+}
 
 int main() {
     testConstructor();
     testGetStatus();
+    testTick();
 }
